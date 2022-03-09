9922e10592669184ce310a75e869f88afb190ec1
Stefan Bertos <stefan.bertos@gmail.com>
2022-03-09 08:15:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from google.cloud import logging
-
-
 def hello_world(request):
     """Responds to any HTTP request.
     Args:
@@ -10,23 +7,11 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    # Instantiates a client
-    logging_client = logging.Client()
-    # The name of the log to write to
-    log_name = "hello-function-log"
-    # Selects the log to write to
-    logger = logging_client.logger(log_name)
-    # Writes the log entry simple text
-    logger.log_text("My function is starting")
-
     request_json = request.get_json()  # see https://flask.palletsprojects.com/en/2.0.x/api/ get_json Parse data as JSON
-
-    logger.log_text("Request headers:" + str(request.headers))
 
     if request.args and 'message' in request.args:
         return request.args.get('message')
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        logger.log_text("sending the default response as no input was provided", severity="WARNING")
         return f'Hello World!'
