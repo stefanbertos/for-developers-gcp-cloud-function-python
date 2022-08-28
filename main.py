1d7a61a05e8ab2c26f5e97ec323b58fcd33ce9b0
Stefan Bertos <stefan.bertos@gmail.com>
2022-08-28 17:20:12 +0200

    demo

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    request_json = request.get_json()  # see https://flask.palletsprojects.com/en/2.0.x/api/ get_json Parse data as JSON
+    request_json = request.get_json(force=True, silent=True, cache=True)  # see https://flask.palletsprojects.com/en/2.0.x/api/ get_json Parse data as JSON
+    #in v.2.1 of the Flask framework they changed the impl of the get_json and if the content type if not provided it returns http 400 - BAD Requests, therefore we are usong now force=true and silent=true
 
     if request.args and 'message' in request.args:
         return request.args.get('message')
